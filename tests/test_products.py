7041cb60dfb8f443ae8a17a8f49b40dc9acc165d
Ex_person <baranovs11@yandex.ru>
2024-11-24 02:16:31 +0300

    Реализация функций + теста для Product

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,5 +1,50 @@
+from src.products import Product
+import pytest
+from unittest.mock import patch
+
+
 def test_product(product) -> None:
     assert product.name == "Apple"
     assert product.description == "Golden apple"
     assert product.price == 50.1
     assert product.quantity == 10
+
+
+def test_new_product():
+    new_product = Product.new_product({"name":"Orange", "description":"Krasnodar Orange", "price":50.1, "quantity":10})
+    assert new_product.name == "Orange"
+    assert new_product.description == "Krasnodar Orange"
+    assert new_product.price == 50.1
+    assert new_product.quantity == 10
+
+
+def test_new_product_product_exists():
+    new_product = Product.new_product(
+        {"name": "Ananas", "description": "Krasnodar Ananas", "price": 50.1, "quantity": 10})
+    assert new_product.quantity == 10
+    assert new_product.price == 50.1
+    new_product = Product.new_product(
+        {"name": "Ananas", "description": "Krasnodar Ananas", "price": 60.1, "quantity": 5})
+    assert new_product.quantity == 15
+    assert new_product.price == 60.1
+
+
+@patch("builtins.input")
+def test_price(mock_input):
+    new_product = Product.new_product(
+        {"name": "Lemon", "description": "Krasnodar Lemon", "price": 50, "quantity": 10})
+    assert new_product.price == 50
+
+    new_product.price = 60
+    assert new_product.price == 60
+
+    with pytest.raises(ValueError, match="Цена не должна быть нулевая или отрицательная"):
+        new_product.price = 0
+
+    mock_input.return_value = "y"
+    new_product.price = 50
+    assert new_product.price == 50
+
+    mock_input.return_value = "n"
+    new_product.price = 40
+    assert new_product.price == 50
